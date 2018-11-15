13ce9492d14881ef2171619fb1abe88e074a56aa
muhsinSutanto <sutantomuhsin@gmail.com>
2018-11-15 15:00:07 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
-import { url } from 'inspector';
 
-class  extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
@@ -27,14 +26,24 @@ class  extends Component {
 
     data.append('user_data', JSON.stringify({name: this.state.name, email: this.state.email}))
     data.append('user_image', this.state.image)
+
+    axios.post('http://localhost:3001/api/users', data ,{
+    headers: {
+      'Content-Type' : 'multipart/form-data'
+    }
+  })
+  .then(res => console.log(res.data.user))
+  .catch(err => console.log(err))
   }
+
+  
 
   render() { 
     return ( 
       <div>
         <input type="text" name="name" onChange={this.handleChange} value={this.state.name} />
         <input type="email" name="email" onChange={this.handleChange} value={this.state.email}/>
-        <input type="file" name="image" onChange-={this.handleImageChange}/>
+        <input type="file" name="image" onChange={this.handleImageChange}/>
         <button onClick={this.handleOnSubmit}>submit</button>
       </div>
      );
